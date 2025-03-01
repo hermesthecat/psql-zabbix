58ce2d8cf5a2af305f28786a6f27124842b02611
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-03-01 19:54:19 +0300

    Basitleştirilmiş pCloud yükleme yaklaşımı
    - upload.sh'ı daha kompakt ve doğrudan bir yükleme betiğine dönüştür
    - pCloud yükleme işlemini ayrı bir betikle gerçekleştir
    - Geçici dosya adlandırma ve yedekleme için tarih bazlı isimlendirme ekle
    - Gereksiz hata ayıklama ve log mekanizmalarını kaldır

--- pcloud.sh
+++ pcloud.sh
@@ -16,6 +16,12 @@ rm -f "$LOG_FILE"
 # Env dosyasını yükle
 source "$ENV_FILE"
 
-cp /home/zipbackup/DB-Backup_All.tar.gz /home/zipbackup/DB-Backup_All.$(date +%y)$(date +%m)$(date +%d).tar.gz
-./pcloud.sh /home/zipbackup/DB-Backup_All.$(date +%y)$(date +%m)$(date +%d).tar.gz PCLOUD_FOLDER_ID
-rm -rf /home/zipbackup/DB-Backup_All.*
+
+strAuth=$(curl -s --location --request GET 'https://eapi.pcloud.com/userinfo?getauth=1&username=PCLOUD_USERNAME&password=PCLOUD_PASSWORD' | jq -r ".auth")
+echo $strAuth
+
+
+
+curl --location --request POST "https://eapi.pcloud.com/uploadfile?auth=$strAuth" \
+--form "folderid=$2" \
+--form "file=@$1"
