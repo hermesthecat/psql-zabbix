9d114a34bb996664118d6095f69911628579757a
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-03-02 17:00:37 +0300

    pCloud betik güncellemesi: Dosya yükleme ve log mekanizmasını geliştir
    - Dosya yükleme sonrası log dosyasına kayıt eklendi
    - Yükleme sonrası log mekanizması basitleştirildi
    - Dosya yükleme işleminin izlenebilirliği artırıldı

--- pcloud.sh
+++ pcloud.sh
@@ -16,14 +16,20 @@ rm -f "$LOG_FILE"
 # Env dosyasını yükle
 source "$ENV_FILE"
 
+# pCloud kimlik doğrulama
 strAuth=$(curl -s --location --request GET "https://eapi.pcloud.com/userinfo?getauth=1&username=$PCLOUD_USERNAME&password=$PCLOUD_PASSWORD" | jq -r ".auth")
 echo $strAuth
 
+# pCloud kimlik doğrulama başarısızsa
 if [ -z "$strAuth" ] || [ "$strAuth" == "null" ]; then
     echo "HATA: pCloud kimlik doğrulama başarısız oldu!"
     exit 1
 fi
 
+# pCloud'a dosya yükle
 curl --location --request POST "https://eapi.pcloud.com/uploadfile?auth=$strAuth" \
 --form "folderid=$2" \
 --form "file=@$1"
+
+# Dosya yükleme sonrası log dosyasına yaz
+echo "Dosya yüklendi: $1" >> "$LOG_FILE"
